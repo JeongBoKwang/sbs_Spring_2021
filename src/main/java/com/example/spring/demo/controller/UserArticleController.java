b304cdba78ca3aaf47115b2949a2c7dd92cf32a7
JeongBoKwang <jbk601@naver.com>
2021-11-09 18:02:27 +0900

    게시물 수정방법

---
+++ src/main/java/com/example/spring/demo/controller/UserArticleController.java
@@ -0,0 +1,116 @@
+package com.example.spring.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.exam.demo.vo.Article;
+
+
+@Controller
+public class UserArticleController {
+	//인스턴스 변수
+	private int articleLastId;
+	private List<Article> articles;
+	
+	//생성자
+	public UserArticleController() {
+		articleLastId = 0;
+		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	//서비스 메서드 시작
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			writeArticle(title,body);
+		}
+	}
+	
+	public Article writeArticle(String title, String body) {
+		int id = articleLastId +1;
+		Article article = new Article(id, title, body);
+		
+		articles.add(article);
+		articleLastId = id;
+		
+		return article;
+	}
+	
+	private Article getArticle(int id) {
+		for(Article article : articles) {
+			if(article.getId()==id) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+	}
+	
+	//서비스 메서드 끝
+	
+	//액션 메서드
+	@RequestMapping("/user/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title,body);
+		return article;
+	} 
+	
+	@RequestMapping("/user/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articles;
+	}
+	
+	@RequestMapping("/user/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id +  "존재하지 않습니다.";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 게시물을 삭제했습니다.";
+	}
+	
+	@RequestMapping("/user/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id +  "존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body);
+		
+		return id + "번 게시물을 수정했습니다.";
+	}
+	//액션 메서드
+
+
+
+}
