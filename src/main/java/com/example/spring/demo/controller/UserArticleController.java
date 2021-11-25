215bb3982782c28ade2b7a98659337a465ffe6b4
JeongBoKwang <jbk601@naver.com>
2021-11-25 20:54:05 +0900

    ArticleService 로 이동

--- src/main/java/com/example/spring/demo/controller/UserArticleController.java
+++ src/main/java/com/example/spring/demo/controller/UserArticleController.java
@@ -17,79 +17,25 @@ public class UserArticleController {
 	//인스턴스 변수 
 	@Autowired
 	private ArticleService articleService;
-	private int articleLastId;
-	private List<Article> articles;
-	
-	//생성자
-	public UserArticleController() {
-		articleLastId = 0;
-		articles = new ArrayList<>();
-		
-		makeTestData();
-	}
-	
-	//서비스 메서드 시작
-	private void makeTestData() {
-		for(int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			writeArticle(title,body);
-		}
-	}
-	
-	public Article writeArticle(String title, String body) {
-		int id = articleLastId +1;
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		articleLastId = id;
-		
-		return article;
-	}
-	
-	private Article getArticle(int id) {
-		for(Article article : articles) {
-			if(article.getId()==id) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-	
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		
-		articles.remove(article);
-	}
-	
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-	}
-	
-	//서비스 메서드 끝
 	
 	//액션 메서드
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		Article article = writeArticle(title,body);
+		Article article = articleService.writeArticle(title,body);
 		return article;
 	} 
 	
 	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articleService.getArticles();
 	}
 	
 	@RequestMapping("/user/article/getArticle")
 	@ResponseBody
 	public Object getArticleAction(int id) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
@@ -100,13 +46,13 @@ public class UserArticleController {
 	@RequestMapping("/user/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id +  "존재하지 않습니다.";
 		}
 		
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		
 		return id + "번 게시물을 삭제했습니다.";
 	}
@@ -114,13 +60,13 @@ public class UserArticleController {
 	@RequestMapping("/user/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id +  "존재하지 않습니다.";
 		}
 		
-		modifyArticle(id, title, body);
+		articleService.modifyArticle(id, title, body);
 		
 		return id + "번 게시물을 수정했습니다.";
 	}
--- src/main/java/com/example/spring/demo/controller/UserHomeController.java
+++ src/main/java/com/example/spring/demo/controller/UserHomeController.java
@@ -17,86 +17,10 @@ import lombok.NoArgsConstructor;
 
 @Controller
 public class UserHomeController {
-	@RequestMapping("/user/home/getString")
+	@RequestMapping("/user/home/main")
 	@ResponseBody
 	public String getString() {
-		return "Hi";
+		return "안녕하세요.";
 	}
-	
-	@RequestMapping("/user/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	
-	@RequestMapping("/user/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	
-	@RequestMapping("/user/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	
-	@RequestMapping("/user/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	
-	@RequestMapping("/user/home/getChar")
-	@ResponseBody
-	public char getChar() {
-		return 'a';
-	}
-	
-	@RequestMapping("/user/home/getMap")
-	@ResponseBody
-	public Map getMap() {
-		Map<String, Object> map = new HashMap<>();
-		
-		map.put("철수의 나이", 22);
-		map.put("영희의 나이", 33);
-		
-		return map;
-	}
-	
-	@RequestMapping("/user/home/getList")
-	@ResponseBody
-	public List getList() {
-		List<String> list = new ArrayList<>();
-		
-		list.add("철수의 나이");
-		list.add("영희의 나이");
-		
-		return list;
-	}
-	
-	@RequestMapping("/user/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		
-		Article article = new Article(1, "제목1", "내용1");
-		Article article1 = new Article();
-		
-		return article;
-	}
-	
-	@RequestMapping("/user/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		
-		Article article1 = new Article(1, "제목1", "내용1");
-		Article article2 = new Article(2, "제목2", "내용2");
-		
-		List<Article> list = new ArrayList<>();
-		
-		list.add(article1);
-		list.add(article2);
-		
-		return list;
-	}
+
 }
