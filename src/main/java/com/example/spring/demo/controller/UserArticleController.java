6222454de776c9bfc4400b43d1e7c77b3c521ec9
JeongBoKwang <jbk601@naver.com>
2021-11-28 21:02:08 +0900

    ArticleRepository로 이동

--- src/main/java/com/example/spring/demo/controller/UserArticleController.java
+++ src/main/java/com/example/spring/demo/controller/UserArticleController.java
@@ -1,6 +1,5 @@
 package com.example.spring.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,67 +10,64 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.spring.demo.service.ArticleService;
 import com.sbs.exam.demo.vo.Article;
 
-
 @Controller
 public class UserArticleController {
-	//인스턴스 변수 
+	// 인스턴스 변수
 	@Autowired
 	private ArticleService articleService;
-	
-	//액션 메서드
+
+	// 액션 메서드
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		Article article = articleService.writeArticle(title,body);
+		Article article = articleService.writeArticle(title, body);
 		return article;
-	} 
-	
+	}
+
 	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articleService.getArticles();
 	}
-	
+
 	@RequestMapping("/user/article/getArticle")
 	@ResponseBody
 	public Object getArticleAction(int id) {
 		Article article = articleService.getArticle(id);
-		
-		if(article == null) {
+
+		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
 		return article;
 	}
-	
+
 	@RequestMapping("/user/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
 		Article article = articleService.getArticle(id);
-		
-		if(article == null) {
-			return id +  "존재하지 않습니다.";
+
+		if (article == null) {
+			return id + "존재하지 않습니다.";
 		}
-		
+
 		articleService.deleteArticle(id);
-		
+
 		return id + "번 게시물을 삭제했습니다.";
 	}
-	
+
 	@RequestMapping("/user/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
-		
-		if(article == null) {
-			return id +  "존재하지 않습니다.";
+
+		if (article == null) {
+			return id + "존재하지 않습니다.";
 		}
-		
+
 		articleService.modifyArticle(id, title, body);
-		
+
 		return id + "번 게시물을 수정했습니다.";
 	}
-	//액션 메서드
-
-
+	// 액션 메서드
 
 }
