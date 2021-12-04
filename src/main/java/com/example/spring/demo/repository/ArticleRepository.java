3e8f7ad55e18d864fc63867a441180e36df433f2
JeongBoKwang <jbk601@naver.com>
2021-12-04 14:42:54 +0900

    게시물 mapper xml 파일 분리 및 게시물 수정시 입력정보만 수정

--- src/main/java/com/example/spring/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/spring/demo/repository/ArticleRepository.java
@@ -1,28 +1,23 @@
 package com.example.spring.demo.repository;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import com.sbs.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	
-	//INSERT INTO article WHERE id = ?
-	public Article writeArticle(String title, String body);
-	
-	@Select("SELECT * FROM article WHERE id = #{id}")
+
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
+
 	public Article getArticle(@Param("id") int id);
-	
-	//DELETE FROM article WHERE id = ?
-	public void deleteArticle(int id);
-	
-	//UPDATE article SET title = ?, body = ? WHERE id = ?
+
+	public void deleteArticle(@Param("id") int id);
+
 	public void modifyArticle(int id, String title, String body);
-	
-	//SELECT * FROM article ORDER BY id DESC;
+
 	public List<Article> getArticles();
+
+	public int getLastInsertId();
 }
